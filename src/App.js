d5db72f43c14dde1126bf5bf34cb085ea7d8cd93
Edouard Demotes-Mainard <e.demotes@gmail.com>
2021-03-24 11:40:55 -0400

    chore: add hapify templates

--- src/App.js
+++ src/App.js
@@ -3,12 +3,190 @@ import * as React from "react";
 import { Admin, Resource } from 'react-admin';
 import dataProvider from "./data/AppDataProvider";
 
-import {PostCreate, PostEdit, PostList, PostShow} from './resources/Post';
+import { BookmarkList, BookmarkShow } from './resources/Bookmark/Read.js';
+import { BookmarkCreate, BookmarkEdit } from './resources/Bookmark/Write.js';
+import { CommentList, CommentShow } from './resources/Comment/Read.js';
+import { CommentCreate, CommentEdit } from './resources/Comment/Write.js';
+import { ConversationList, ConversationShow } from './resources/Conversation/Read.js';
+import { ConversationCreate, ConversationEdit } from './resources/Conversation/Write.js';
+import { ConversationReportList, ConversationReportShow } from './resources/ConversationReport/Read.js';
+import { ConversationReportCreate, ConversationReportEdit } from './resources/ConversationReport/Write.js';
+import { MenuList, MenuShow } from './resources/Menu/Read.js';
+import { MenuCreate, MenuEdit } from './resources/Menu/Write.js';
+import { MenuItemList, MenuItemShow } from './resources/MenuItem/Read.js';
+import { MenuItemCreate, MenuItemEdit } from './resources/MenuItem/Write.js';
+import { MenuPartList, MenuPartShow } from './resources/MenuPart/Read.js';
+import { MenuPartCreate, MenuPartEdit } from './resources/MenuPart/Write.js';
+import { MessageList, MessageShow } from './resources/Message/Read.js';
+import { MessageCreate, MessageEdit } from './resources/Message/Write.js';
+import { OrderList, OrderShow } from './resources/Order/Read.js';
+import { OrderCreate, OrderEdit } from './resources/Order/Write.js';
+import { PlaceList, PlaceShow } from './resources/Place/Read.js';
+import { PlaceCreate, PlaceEdit } from './resources/Place/Write.js';
+import { PlaceCategoryList, PlaceCategoryShow } from './resources/PlaceCategory/Read.js';
+import { PlaceCategoryCreate, PlaceCategoryEdit } from './resources/PlaceCategory/Write.js';
+import { ProductList, ProductShow } from './resources/Product/Read.js';
+import { ProductCreate, ProductEdit } from './resources/Product/Write.js';
+import { RestaurantList, RestaurantShow } from './resources/Restaurant/Read.js';
+import { RestaurantCreate, RestaurantEdit } from './resources/Restaurant/Write.js';
+import { RestaurantCategoryList, RestaurantCategoryShow } from './resources/RestaurantCategory/Read.js';
+import { RestaurantCategoryCreate, RestaurantCategoryEdit } from './resources/RestaurantCategory/Write.js';
+import { ServiceList, ServiceShow } from './resources/Service/Read.js';
+import { ServiceCreate, ServiceEdit } from './resources/Service/Write.js';
+import { ShoppingCartItemList, ShoppingCartItemShow } from './resources/ShoppingCartItem/Read.js';
+import { ShoppingCartItemCreate, ShoppingCartItemEdit } from './resources/ShoppingCartItem/Write.js';
+import { UserList, UserShow } from './resources/User/Read.js';
+import { UserCreate, UserEdit } from './resources/User/Write.js';
+import { WishlistList, WishlistShow } from './resources/Wishlist/Read.js';
+import { WishlistCreate, WishlistEdit } from './resources/Wishlist/Write.js';
 
 const App = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} show={PostShow} />
-    </Admin>
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="bookmark"
+      options={{ label: 'Bookmark' }}
+      list={BookmarkList}
+      create={BookmarkCreate}
+      edit={BookmarkEdit}
+      show={BookmarkShow}
+    />
+    <Resource
+      name="comment"
+      options={{ label: 'Comment' }}
+      list={CommentList}
+      create={CommentCreate}
+      edit={CommentEdit}
+      show={CommentShow}
+    />
+    <Resource
+      name="conversation"
+      options={{ label: 'Conversation' }}
+      list={ConversationList}
+      create={ConversationCreate}
+      edit={ConversationEdit}
+      show={ConversationShow}
+    />
+    <Resource
+      name="conversationReport"
+      options={{ label: 'Conversation Report' }}
+      list={ConversationReportList}
+      create={ConversationReportCreate}
+      edit={ConversationReportEdit}
+      show={ConversationReportShow}
+    />
+    <Resource
+      name="menu"
+      options={{ label: 'Menu' }}
+      list={MenuList}
+      create={MenuCreate}
+      edit={MenuEdit}
+      show={MenuShow}
+    />
+    <Resource
+      name="menuItem"
+      options={{ label: 'Menu Item' }}
+      list={MenuItemList}
+      create={MenuItemCreate}
+      edit={MenuItemEdit}
+      show={MenuItemShow}
+    />
+    <Resource
+      name="menuPart"
+      options={{ label: 'Menu Part' }}
+      list={MenuPartList}
+      create={MenuPartCreate}
+      edit={MenuPartEdit}
+      show={MenuPartShow}
+    />
+    <Resource
+      name="message"
+      options={{ label: 'Message' }}
+      list={MessageList}
+      create={MessageCreate}
+      edit={MessageEdit}
+      show={MessageShow}
+    />
+    <Resource
+      name="order"
+      options={{ label: 'Order' }}
+      list={OrderList}
+      create={OrderCreate}
+      edit={OrderEdit}
+      show={OrderShow}
+    />
+    <Resource
+      name="place"
+      options={{ label: 'Place' }}
+      list={PlaceList}
+      create={PlaceCreate}
+      edit={PlaceEdit}
+      show={PlaceShow}
+    />
+    <Resource
+      name="placeCategory"
+      options={{ label: 'Place Category' }}
+      list={PlaceCategoryList}
+      create={PlaceCategoryCreate}
+      edit={PlaceCategoryEdit}
+      show={PlaceCategoryShow}
+    />
+    <Resource
+      name="product"
+      options={{ label: 'Product' }}
+      list={ProductList}
+      create={ProductCreate}
+      edit={ProductEdit}
+      show={ProductShow}
+    />
+    <Resource
+      name="restaurant"
+      options={{ label: 'Restaurant' }}
+      list={RestaurantList}
+      create={RestaurantCreate}
+      edit={RestaurantEdit}
+      show={RestaurantShow}
+    />
+    <Resource
+      name="restaurantCategory"
+      options={{ label: 'Restaurant Category' }}
+      list={RestaurantCategoryList}
+      create={RestaurantCategoryCreate}
+      edit={RestaurantCategoryEdit}
+      show={RestaurantCategoryShow}
+    />
+    <Resource
+      name="service"
+      options={{ label: 'Service' }}
+      list={ServiceList}
+      create={ServiceCreate}
+      edit={ServiceEdit}
+      show={ServiceShow}
+    />
+    <Resource
+      name="shoppingCartItem"
+      options={{ label: 'Shopping Cart Item' }}
+      list={ShoppingCartItemList}
+      create={ShoppingCartItemCreate}
+      edit={ShoppingCartItemEdit}
+      show={ShoppingCartItemShow}
+    />
+    <Resource
+      name="user"
+      options={{ label: 'User' }}
+      list={UserList}
+      create={UserCreate}
+      edit={UserEdit}
+      show={UserShow}
+    />
+    <Resource
+      name="wishlist"
+      options={{ label: 'Wishlist' }}
+      list={WishlistList}
+      create={WishlistCreate}
+      edit={WishlistEdit}
+      show={WishlistShow}
+    />
+  </Admin>
 );
 
 export default App;
--- src/data/DexieDataProvider.js
+++ src/data/DexieDataProvider.js
@@ -6,7 +6,6 @@ import {
   CREATE,
   UPDATE,
   DELETE,
-  // fetchUtils,
 } from "react-admin";
 import Dexie from "dexie";
 
@@ -20,78 +19,105 @@ export default (databaseName, databaseVersion, databaseStores) => {
    * @param {Object} payload Request parameters. Depends on the request type
    * @returns {Promise} the Promise for response
    */
-  return (type, resource, params) =>
-    new Promise((resolve, reject) => {
-      db.open();
+  return async function (type, resource, params) {
+    await db.open();
+    const collection = db.table(resource);
 
-      switch (type) {
-        case DELETE:
-          db.table(resource)
-            .delete(parseInt(params.id))
-            .then((data) => {
-              resolve({ data });
-            });
-          break;
+    switch (type) {
+      case DELETE:
+        return await remove(collection, params);
+      case GET_ONE:
+        return await one(collection, params);
+      case CREATE:
+        return await add(collection, params);
+      case UPDATE:
+        return await update(collection, params);
+      case GET_LIST:
+        return await list(collection, params);
+      case GET_MANY:
+      case GET_MANY_REFERENCE:
+        return await many(collection, params);
+      default:
+        return null;
+    }
+  }
 
-        case GET_ONE:
-          db.table(resource)
-            .get(parseInt(params.id))
-            .then((data) => {
-              resolve({ data });
-            });
-          break;
-
-        case CREATE:
-          db.table(resource)
-            .add(params.data)
-            .then((id) => {
-              resolve(params);
-            });
-          break;
-
-        case UPDATE:
-          db.table(resource)
-            .update(params.data.id, params.data)
-            .then((updated) => {
-              resolve(params);
-            });
-
-          break;
-
-        case GET_LIST:
-        case GET_MANY:
-        case GET_MANY_REFERENCE:
-          const { page, perPage } = params.pagination;
-          const { field, order } = params.sort;
-
-          const offset = page > 1 ? (page - 1) * perPage : 0;
-
-          db.table(resource).count((count) => {
-            let collection = db.table(resource);
-            collection = collection.orderBy(field);
-
-            if (order.toLowerCase() === "desc") {
-              collection = collection.reverse();
-            }
-
-            collection
-              .offset(offset)
-              .limit(perPage)
-              .toArray()
-              .then((data) => {
-                resolve({
-                  data: data,
-                  total: count,
-                  page: page,
-                  totalCount: count,
-                });
-              });
-          });
-
-          break;
-
-        default:
-          break;
-      }
-    });
 };
+
+async function remove(collection, params) {
+  const data = await collection.delete(parseInt(params.id));
+  return { data };
+}
+
+async function one(collection, params) {
+  const data = await collection.get(parseInt(params.id));
+  return { data };
+}
+
+async function add(collection, params) {
+  params.data.id = await collection.add(params.data);
+  return params;
+}
+
+async function update(collection, params) {
+  await collection.update(params.data.id, params.data);
+  return params;
+}
+
+async function list(collection, params) {
+
+  let query = collection;
+  convertBooleans(params.filter);
+
+  // Filtering
+  if (Object.keys(params.filter).length > 0) {
+    query = query.where(params.filter);
+  }
+  // Sort
+  else if (params.sort) {
+    const { field, order } = params.sort;
+    query = query.orderBy(field);
+    if (order.toLowerCase() === "desc") {
+      query = query.reverse();
+    }
+  }
+
+  // Count
+  const count = await query.count();
+
+  // Pagination
+  if (params.pagination) {
+    const { page, perPage } = params.pagination;
+    const offset = page > 1 ? (page - 1) * perPage : 0;
+    query = query.offset(offset).limit(perPage);
+  }
+
+  const data = await query.toArray();
+
+  return {
+    data: data,
+    total: count,
+    page: params.pagination ? params.pagination.page : 1,
+    totalCount: count,
+  };
+}
+
+async function many(collection, params) {
+  const count = await collection.count();
+  const data = await collection.toArray();
+
+  return {
+    data: data,
+    total: count,
+    page: 0,
+    totalCount: count,
+  };
+}
+
+function convertBooleans(data) {
+  Object.keys(data).forEach(key => {
+    if (typeof data[key] === 'boolean') {
+      data[key] = data[key] ? 1 : 0;
+    }
+  });
+}
